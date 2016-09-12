cf91fce9f058f60176c77600d9b64bd1d85c7ab9
Bob <22376415@qq.com>
2016-09-12 11:38:56 +0800

    add receiver test

--- src/test/java/com/baogg/webdriver/FirstAutomationTest.java
+++ src/test/java/com/baogg/webdriver/FirstAutomationTest.java
@@ -5,6 +5,7 @@ package com.baogg.webdriver;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -13,18 +14,24 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
+
 import org.junit.*;
+
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
+
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
  * Created by ONUR BASKIRT on 26.08.2015.
  */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class FirstAutomationTest {
 
     private WebDriver driver;
@@ -34,13 +41,22 @@ public class FirstAutomationTest {
 
     @Before
     public void setUp() throws Exception {
+
+        /*
+        ChromeOptions options = new ChromeOptions();
+        options.setBinary("/usr/bin/google-chrome");
+
+        DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+        capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+        */
+
         driver = new FirefoxDriver();
-            baseUrl = "https://123.207.122.202:3002/1/#/rs/login";
+        baseUrl = "https://localhost:3002/1/#/rs/login";
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
 
     @Test
-    public void testSelenium2() throws Exception {
+    public void testReferralSmartASender() throws Exception {
         driver.get(baseUrl + "/1/#/rs/login");
         driver.findElement(By.id("email")).clear();
         driver.findElement(By.id("email")).sendKeys("22376415@qq.com");
@@ -59,7 +75,7 @@ public class FirstAutomationTest {
         //input[@ng-model="theReferal.customerPhone"]
         driver.findElement(By.xpath("//div[contains(@class, 'btn-next')]")).click();
         driver.findElement(By.xpath("//div[@ng-model='theReferal.rePartner.selected']//span[@ng-click='$select.activate()']")).click();
-        driver.findElement(By.xpath("//div[@ng-model='theReferal.rePartner.selected']//input")).sendKeys("frimann2test@gmail.com");
+        driver.findElement(By.xpath("//div[@ng-model='theReferal.rePartner.selected']//input")).sendKeys("ylb2016@126.com");
         driver.findElement(By.xpath("//input[@ng-model='theReferal.newReceiverName']")).sendKeys("Test Bob");
         driver.findElement(By.xpath("//input[@ng-model='theReferal.newReceiverPhone']")).sendKeys("0412987654");
 
@@ -69,9 +85,98 @@ public class FirstAutomationTest {
         driver.findElement(By.xpath("//button[@ng-disabled='!isAgree']")).click();
         //driver.findElement(By.xpath("//button[2]")).click();
         driver.findElement(By.cssSelector("button.confirm")).click();
+        waitSweetAlert();
+
+
+
         driver.findElement(By.xpath("//li[@ui-sref='app-rs.profile.info']")).click();
         driver.findElement(By.xpath("//button[@ng-click='logout()']")).click();
 
+
+    }
+
+
+    @Test
+    public void testReferralSmartBReceiver() throws Exception {
+
+        driver.get(baseUrl + "/1/#/rs/login");
+        driver.findElement(By.id("email")).clear();
+        driver.findElement(By.id("email")).sendKeys("frimann2test@gmail.com");
+        driver.findElement(By.id("pwd")).clear();
+        driver.findElement(By.id("pwd")).sendKeys("123456");
+        driver.findElement(By.id("btnLogin")).click();
+
+        Thread.sleep(5000);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        (new WebDriverWait(driver, 10)).until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//table[@id=\"receiverDashboard\"]/tbody/tr")));
+        driver.findElement(By.xpath("//table[@id=\"receiverDashboard\"]/tbody/tr")).click();
+
+        Thread.sleep(5000);
+        (new WebDriverWait(driver, 10)).until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#block_status_Contacted_Client > div.rs-timeline-content > div.rs-subject")));
+
+        driver.findElement(By.cssSelector("#block_status_Contacted_Client > div.rs-timeline-content > div.rs-subject")).click();
+        driver.findElement(By.cssSelector("button.confirm")).click();
+
+        waitSweetAlert();
+        Thread.sleep(5000);
+
+        driver.findElement(By.cssSelector("#block_status_Appointment_Made_or_Lead_Closed > div.rs-timeline-content > div.rs-subject > p.smwidth.ng-binding")).click();
+        driver.findElement(By.cssSelector("button.confirm")).click();
+        waitSweetAlert();
+        Thread.sleep(5000);
+
+        driver.findElement(By.cssSelector("#block_status_Appointment_Completed > div.rs-timeline-content > div.rs-subject")).click();
+        driver.findElement(By.cssSelector("button.confirm")).click();
+        waitSweetAlert();
+        Thread.sleep(5000);
+
+
+        driver.findElement(By.cssSelector("#block_status_Application_Received > div.rs-timeline-content > div.rs-subject")).click();
+        driver.findElement(By.cssSelector("button.confirm")).click();
+        waitSweetAlert();
+        Thread.sleep(5000);
+
+        driver.findElement(By.cssSelector("#block_status_Application_Submitted > div.rs-timeline-content > div.rs-subject")).click();
+        driver.findElement(By.cssSelector("button.confirm")).click();
+        waitSweetAlert();
+        Thread.sleep(5000);
+
+        driver.findElement(By.cssSelector("#block_status_Application_Conditional_Approval > div.rs-timeline-content > div.rs-subject")).click();
+        driver.findElement(By.cssSelector("button.confirm")).click();
+        waitSweetAlert();
+        Thread.sleep(5000);
+
+        driver.findElement(By.cssSelector("#block_status_Valuation > div.rs-timeline-content > div.rs-subject")).click();
+        driver.findElement(By.cssSelector("button.confirm")).click();
+        waitSweetAlert();
+        Thread.sleep(5000);
+
+
+        driver.findElement(By.cssSelector("#block_status_Application_Formal_Approval > div.rs-timeline-content > div.rs-subject > p.smwidth.ng-binding")).click();
+
+        driver.findElement(By.cssSelector("button.confirm")).click();
+        waitSweetAlert();
+        Thread.sleep(5000);
+
+
+        driver.findElement(By.cssSelector("#block_status_Settlement_Booked > div.rs-timeline-content > div.rs-subject")).click();
+        driver.findElement(By.cssSelector("button.confirm")).click();
+        waitSweetAlert();
+        Thread.sleep(5000);
+
+        driver.findElement(By.cssSelector("#block_status_Settlement > div.rs-timeline-content > div.rs-subject > p.smwidth.ng-binding")).click();
+        driver.findElement(By.cssSelector("button.confirm")).click();
+        waitSweetAlert();
+    }
+
+    public void waitSweetAlert() {
+
+        By loadingImage = By.cssSelector(".sweet-overlay");
+
+        WebDriverWait wait = new WebDriverWait(driver, 5);
+
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(loadingImage));
     }
 
     @After
